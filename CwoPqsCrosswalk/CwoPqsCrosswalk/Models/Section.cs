8cfc0a3c57cb3a87fbb30fb57f404bfe76cf862b
Lindsay Spencer <lindsaymspencer@outlook.com>
2020-08-17 20:34:35 -0500

    Added file forgot in last checkin.

---
+++ CwoPqsCrosswalk/CwoPqsCrosswalk/Models/Section.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace CwoPqsCrosswalk.Models
+{
+    public class Section
+    {
+        public string Name { get; set; }
+        public string Number { get; set; }
+        public Dictionary<string, bool> Signatures { get; set; }
+    }
+}
