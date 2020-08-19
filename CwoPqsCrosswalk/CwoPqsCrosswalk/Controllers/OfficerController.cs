11f25d486d4784d9cfceb1e9d23c89d82f2e580a
Lindsay Spencer <lindsaymspencer@outlook.com>
2020-08-19 09:23:06 -0500

    Changed naming convention from 2B to TwoBravo. Added Officer, including showing signatures from 2B. Changed CSV to semi-colon seperated values.

---
+++ CwoPqsCrosswalk/CwoPqsCrosswalk/Controllers/OfficerController.cs
@@ -0,0 +1,16 @@
+﻿using CwoPqsCrosswalk.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CwoPqsCrosswalk.Controllers
+{
+    public class OfficerController : Controller
+    {
+        // 
+        // GET: /Officer/
+
+        public IActionResult Index()
+        {
+            return View(new Officer());
+        }
+    }
+}
---
+++ CwoPqsCrosswalk/CwoPqsCrosswalk/Models/Officer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CwoPqsCrosswalk.Models
+{
+    public class Officer
+    {
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; } = "Lindsay";
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; } = "Spencer";
+
+        public string Rank { get; set; } = "LTJG";
+
+        public CwoPqs TwoBravo { get; set; } = CwoPqs.NewTwoBravo();
+
+    }
+}
